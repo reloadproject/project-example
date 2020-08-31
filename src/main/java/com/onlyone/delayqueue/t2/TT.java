959257995345c4ef4c6f4ea14773169af51d0a1f
onlyone <onlyone@192.168.0.105>
2020-08-31 09:13:47 +0800

    函数式编程是如何提升代码的扩展性

---
+++ src/main/java/com/onlyone/delayqueue/t2/TT.java
@@ -0,0 +1,15 @@
+package com.onlyone.delayqueue.t2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class TT {
+
+    public static void main(String[] args) {
+        List<String> lists = Arrays.asList("Hello", "World");
+        lists.stream().flatMap(word -> Stream.of(word.split("")))
+                .distinct()
+                .forEach(System.out::println);
+    }
+}
